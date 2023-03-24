905234a1bf376f32da162ceca2ed4421cec21743
zqx <114729040@qq.com>
2023-03-24 09:14:33 +0800

    Java高级

---
+++ ch03/src/test00/demo05/MyArrayList.java
@@ -0,0 +1,31 @@
+package test00.demo05;
+
+/**
+ * 模拟 ArrayList - ArrayList 本质上是对 数组进行封装
+ *
+ *
+ * @Date 2023-03-24
+ * @Author zqx
+ */
+public class MyArrayList {
+    /**
+     * 集合的初始容量 - 常量
+     */
+    private static final int DEFAULT_CAPACITY = 10;
+
+    /**
+     * 集合封装的数组 - 真正意义存储数据的空间
+     */
+    private Object[] elementData;
+
+    /**
+     * 集合实际存储元素的大小 - 数组实际的长度
+     */
+    private int size;
+
+
+
+
+
+
+}
